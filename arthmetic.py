1e4f7616686074d71d39632f984a2ec201a74eeb
Varun-nani <nathalavarun@gmail.com>
2024-06-12 14:36:33 +0530

    Create arthmetic.py
    arthmetic operations

---
+++ arthmetic.py
@@ -0,0 +1,23 @@
+a = 7
+b = 2
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)   
