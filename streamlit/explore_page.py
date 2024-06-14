75d5cb445622c981936090e471df070ae4bef2a0
meghanaalakati <22h51a7367@cmrcet.ac.in>
2024-06-14 10:11:42 +0530

    meghana.._sttreamlit

---
+++ streamlit/explore_page.py
@@ -0,0 +1,19 @@
+import streamlit as st
+def show_explore_page():
+    st.title("Explore Software Engineer Salaries")
+    st.write(
+        """
+    ### Stack Overflow Developer Survey 2020
+    """
+    )
+    st.write("""#### Number of Data from different countries""")
+    st.write(
+        """
+    #### Mean Salary Based On Country
+    """
+    )
+    st.write(
+        """
+    #### Mean Salary Based On Experience
+    """
+    )
