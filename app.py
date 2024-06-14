c66f149f9706ecaab5030b3f200b2801b94f7274
AmruthNavaneeth <anavaneeth816@gmail.com>
2024-06-14 23:08:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
-app = Flask(__name__)
 
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
