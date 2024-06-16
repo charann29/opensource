2320f4b25519c2d56bbfa10c4551067f1cf2ce0d
BATTU RISHITHA <22h51a6610@cmrcet.ac.in>
2024-06-16 23:11:02 +0530

    project output

---
+++ apprishi.py
@@ -0,0 +1,13 @@
+import sys
+print(sys.path)
+import streamlit as st
+from predict_page_rishi import show_predict_page
+from explore_page import show_explore_page
+
+
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
