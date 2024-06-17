ab1da1f961a3d47fcc90f56e1237ffcbf9ac0f34
Jatavedreddy <165547397+Jatavedreddy@users.noreply.github.com>
2024-06-17 19:43:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,4 +9,9 @@ int main()
  
     // ith row has i elements
     for (int i = 1; i <= n; i++) {
- 
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+        cout << endl;
+    }
+    return 0;
+}
