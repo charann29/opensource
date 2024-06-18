d49429e125948aa684324f5485846dcfc042b338
charann019 <lsaicharan01@gmail.com>
2024-06-18 14:35:49 +0530

    SCDZgfdhf

--- streamlit-multipage-app-example/MAIN_PAGE.py
+++ streamlit-multipage-app-example/MAIN_PAGE.py
@@ -13,7 +13,7 @@ if "my_input" not in st.session_state:
     st.session_state["my_input"] = ""
 
 my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("Submit")
+submit = st.button("Charan")
 if submit:
     st.session_state["my_input"] = my_input
     st.write("You have entered: ", my_input)
