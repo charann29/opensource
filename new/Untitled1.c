d31bc08c8f11bebc35537e94bcc18d2e26a132c7
MadduriDeekshit <168515593+MadduriDeekshit@users.noreply.github.com>
2024-06-18 09:30:29 -0700

    sid

---
+++ new/Untitled1.c
@@ -0,0 +1,25 @@
+// C++ program to print DFS traversal from
+// a given vertex in a  given graph
+#include <bits/stdc++.h>
+using namespace std;
+
+// Graph class represents a directed graph
+// using adjacency list representation
+class Graph {
+public:
+    map<int, bool> visited;
+    map<int, list<int> > adj;
+
+    // Function to add an edge to graph
+    void addEdge(int v, int w);
+
+    // DFS traversal of the vertices
+    // reachable from v
+    void DFS(int v);
+};
+
+void Graph::addEdge(int v, int w)
+{
+    // Add w to v’s list.
+    adj[v].push_back(w);
+}
