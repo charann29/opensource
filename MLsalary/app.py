5928c9c222efda7a969e150c2b40906fcddf6ea3
Jatavedreddy <165547397+Jatavedreddy@users.noreply.github.com>
2024-06-18 18:38:01 +0530

    c

--- MLsalary/app.py
+++ MLsalary/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
-#
+
 
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
